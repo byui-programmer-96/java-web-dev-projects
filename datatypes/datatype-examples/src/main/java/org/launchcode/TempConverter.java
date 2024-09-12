7b0ae234c5464d195ce86aaaf60875de787201a3
byui-programmer-96 <bam21002@byui.edu>
2024-09-12 11:15:38 -0500

    Exercises work

--- datatypes/datatype-examples/src/main/java/org/launchcode/TempConverter.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/TempConverter.java
@@ -1,19 +1,29 @@
-package org.launchcode;
-
 import java.util.Scanner;
 
-public class TempConverter {
+// Here is an example of a comment.
+
+/* Here is how
+to have
+multi-line
+comments. */
+
+/*
+Or
+like
+this.
+*/
+
+public class HelloWorld {
     public static void main(String[] args) {
-        double fahrenheit;
-        double celsius;
-        Scanner input;
+        Scanner input; // Comments do not have to start at the beginning of a line.
 
         input = new Scanner(System.in);
-        System.out.println("Enter the temperature in Fahrenheit: ");
-        fahrenheit = input.nextDouble();
+        System.out.println("Please enter your first name: ");
+        String name = input.next(); //Declares the 'name' variable and initializes it with text from the command line.
         input.close();
 
-        celsius = (fahrenheit - 32) * 5/9;
-        System.out.println("The temperature in Celsius is: " + celsius + "°C");
+        System.out.println("Hello, " + name + "!");
+
+        // System.out.println("This line will NOT print!");
     }
 }
