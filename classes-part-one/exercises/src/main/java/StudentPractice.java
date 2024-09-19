3d97c091cfe8e1e68a4800c9a1f76e7d180e526b
byui-programmer-96 <bam21002@byui.edu>
2024-09-19 15:09:02 -0500

    Exercises complete with errors

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,16 @@
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //instantiate your Student class below
+
+        Student student = new Student("Cailynn B", 12345, 1, 4.0);
+
+
+        System.out.println("Student Information:");
+        System.out.println("Name: " + student.getName());
+        System.out.println("Student ID: " + student.getStudentId());
+        System.out.println("Number of Credits: " + student.getNumberOfCredits());
+        System.out.println("GPA: " + student.getGpa());
     }
+
+
 }
